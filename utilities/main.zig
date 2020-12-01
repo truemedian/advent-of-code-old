0340829384ad022f23f918dbfaaf5bfad05bbc93
Nameless <truemedian@gmail.com>
2020-11-30 23:21:39 -0600

    update utils

--- utilities/main.zig
+++ utilities/main.zig
@@ -18,3 +18,26 @@ pub fn getFileReader(name: []const u8) !fs.File.Reader {
 
     return file.reader();
 }
+
+pub fn count(data: []const u8, delim: []const u8) usize {
+    var pos: usize = 0;
+    var n: usize = 0;
+
+    while (std.mem.indexOfPos(u8, data, pos, delim)) |next| {
+        pos = next + 1;
+        n += 1;
+    }
+
+    return n;
+}
+
+pub fn split(data: []const u8, delim: []const u8) ![][]const u8 {
+    var ret = try allocator.alloc([]const u8, count(data, delim) + 1);
+    var it = std.mem.split(data, delim);
+
+    for (ret) |_, i| {
+        ret[i] = it.next() orelse unreachable;
+    }
+
+    return ret;
+}
