c5b443b8c2b8bef4318e321446a6d0da09680655
Nameless <truemedian@gmail.com>
2020-12-02 23:45:04 -0600

    refactor: zigify

--- 03/main.zig
+++ 03/main.zig
@@ -6,13 +6,11 @@ fn countTrees(map: [][]const u8, inc_x: usize, int_y: usize) usize {
 
     var row: usize = 0;
     var col: usize = 0;
-    while (row < map.len) {
-        if (map[row][col % map[0].len] == '#') {
-            count += 1;
-        }
-
+    while (row < map.len) : ({
         row += inc_x;
         col += int_y;
+    }) {
+        if (map[row][col % map[0].len] == '#') count += 1;
     }
 
     return count;
@@ -21,9 +19,6 @@ fn countTrees(map: [][]const u8, inc_x: usize, int_y: usize) usize {
 pub fn main() !void {
     const input = try utils.getFileSlice("03/input.txt");
     const inputs = try utils.split(input, "\n");
-
-    var row: usize = 0;
-    var col: usize = 0;
 
     const count11 = countTrees(inputs, 1, 1);
     const count13 = countTrees(inputs, 1, 3);
